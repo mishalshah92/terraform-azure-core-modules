8ccd2f75d541f9191f32daf022a93675626381e7
mishal <mishalshah92@gmail.com>
2021-04-18 23:10:22 +0530

    Updating format.

--- src/default/main.tf
+++ src/default/main.tf
@@ -5,19 +5,19 @@ terraform {
 locals {
 
   default_tags = {
-    Customer = var.customer
-    Env      = var.env
+    Customer   = var.customer
+    Env        = var.env
+    Deployment = var.deployment
 
     Owner = var.owner
     Email = var.email
 
     ResourceGroup = var.resource_group
 
-    Module     = var.module
-    Deployment = var.deployment
-    Branch     = data.git_repository.current_repo.branch
-    Repo       = data.git_repository.current_repo.url
-    Tool       = var.tool
+    Module = var.module
+    Branch = data.git_repository.current_repo.branch
+    Repo   = data.git_repository.current_repo.url
+    Tool   = var.tool
   }
 
   tags = merge(local.default_tags, var.tags)
